c31cd92b7393faddaddc839d633f899fd6e7f8be
YugandharYugandhar <chennuboinayugandhar@gmail.com>
2024-02-26 21:26:21 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
-
-export default function Apple() {
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Signin from './Pages/Signin'
+import Projects from './Pages/Projects'
+import Dashboard from './Pages/Dashboard'
+import DineUp from './Pages/DineUp'
+function App() {
   return (
-    <h>Apple</h>
-  )
+    <div className=''>
+    <BrowserRouter>
+        <Routes>
+        <Route path="/" exact element={<Home/>} />
+        <Route path="/about" exact element={<About/>} />
+        <Route path='/sign_up' element={<DineUp />} />
+        <Route path='/sign_in' element={<Signin />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        </Routes>
+    </BrowserRouter>
+    </div>
+  );
 }
+
+export default App
